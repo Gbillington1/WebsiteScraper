d93dd8215a63972eca5f3e64a2b13e957de7308d
Graham Billington <grahamhbillington@gmail.com>
2020-05-13 14:09:15 -0400

    Added HTML parser that logs out '[og tag]: [value]'

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-var $;
 
 const url = "http://hugopakula.com";
 
 //use axios get html of url 
 axios.get(url)
     .then(response => {
-        console.log(response.data);
+        console.log(response.data)
+        getData(response.data)
     })
     .catch (error => {
         console.error(error);
     })
+
+//parse html with cheerio
+let getData = html => {
+    data = [];
+    const $ = cheerio.load(html);
+    //find all meta tags with the property of ":og", and log them out in an object. (is this an object? Or does it just look like one...)
+    $('meta[property^="og:"]').each(function(i) {
+        console.log($(this).attr("property") + ": " + $(this).attr("content"));
+    })
+}
 
