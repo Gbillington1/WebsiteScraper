0a13d61db7e9e4af33974c6e8a5abe25fb2b865f
Hugo Pakula <hugo@hugopakula.com>
2020-05-13 22:55:24 -0400

    Read index.html for base directory

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs');
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+    fs.readFile("./index.html", function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.write("Not found");
+            res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        }
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
