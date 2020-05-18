4d6c7d136a5b6181a9ad510ed9832be4b946309d
Graham Billington <grahamhbillington@gmail.com>
2020-05-18 01:00:15 -0400

    URL is only scraped if it hasn't been scraped before?  Im confused

--- src/app.js
+++ src/app.js
@@ -16,20 +16,20 @@ client.connect();
 
 //serve html page
 app.get('/', function (req, res) {
-    fs.readFile("./index.html", function(err, data) {
+    fs.readFile("./index.html", function (err, data) {
         if (err) {
             res.writeHead(404);
             res.write("Not found");
             res.end();
         } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
         }
     });
 });
 
-app.post('/scrape', function(req, res) {
+app.post('/scrape', function (req, res) {
     //save URL
     var pattern = /^((http|https|ftp):\/\/)/;
     var scrapeUrl = req.body.url;
@@ -38,18 +38,68 @@ app.post('/scrape', function(req, res) {
         scrapeUrl = "http://" + scrapeUrl;
     }
 
-    scrapes.getScrape(client, scrapeUrl, function(rows, error) {
-        if(typeof error !== "undefined") {
-            res.send({'error': true});
+    scrapes.getScrape(client, scrapeUrl, function (rows, error) {
+        if (typeof error !== "undefined") {
+            res.send({ 'error': true });
 
             return;
         }
 
-        if(rows.length === 0) {
+        // if there are no rows in the db (for this url?) => scrape
+        if (rows.length === 0) {
             // There are no crawls for the provided URL yet
             // TODO: Crawl the url here
-            //  save the crawl data in the database
+            // save the crawl data in the database
+            
+            //use axios get html of url
+            axios.get(scrapeUrl)
+                .then(function (response) {
+                    var urlData = {};
 
+                    // Load our data into cheerio for DOM parsing
+                    const $ = cheerio.load(response.data);
+                    //find all meta tags with the property of ":og", and log them out in an object
+                    $('meta[property^="og:"]').each(function () {
+                        if (typeof urlData.ogData === 'undefined') {
+                            urlData.ogData = {};
+                        }
+
+                        urlData.ogData[$(this).attr("property")] = $(this).attr("content");
+                    });
+
+                    $('head title').each(function () {
+                        // There should only be one <title> tag, use the first as our title value
+                        if (typeof urlData.title === 'undefined') {
+                            urlData.title = $(this).text();
+                        }
+                    });
+
+                    $('h1').each(function (i) {
+
+                        if (typeof urlData.h1 === 'undefined') {
+                            urlData.h1 = {};
+                        }
+
+                        //adds all h1's to urlData
+                        urlData.h1[i] = $(this).text();
+                    })
+                    $('h2').each(function (i) {
+
+                        if (typeof urlData.h2 === 'undefined') {
+                            urlData.h2 = {};
+                        }
+
+                        //adds all h1's to urlData
+                        urlData.h2[i] = $(this).text();
+                    })
+                    
+                    // send data in JSON
+                    // I want to send it to the DB, not to the page
+                    res.send(JSON.stringify(urlData));
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
             return;
         }
 
@@ -57,58 +107,10 @@ app.post('/scrape', function(req, res) {
 
         // There is a crawl
         // TODO: Return the crawl data in the same format
+        // return JSON.stringify(urlData);
     });
 
-    //use axios get html of url
-    axios.get(scrapeUrl)
-        .then(function(response) {
-            var urlData = {};
 
-            // Load our data into cheerio for DOM parsing
-            const $ = cheerio.load(response.data);
-            //find all meta tags with the property of ":og", and log them out in an object
-            $('meta[property^="og:"]').each(function() {
-                if(typeof urlData.ogData === 'undefined') {
-                    urlData.ogData = {};
-                }
-
-                urlData.ogData[$(this).attr("property")] = $(this).attr("content");
-            });
-
-            $('head title').each(function() {
-                // There should only be one <title> tag, use the first as our title value
-                if(typeof urlData.title === 'undefined') {
-                    urlData.title = $(this).text();
-                }
-            });
-
-            // TODO: Add <h1>/<h2> tags to urlData object
-            $('h1').each(function(i) {
-
-                if (typeof urlData.h1 === 'undefined') {
-                    urlData.h1 = {};
-                }
-
-                //adds all h1's to urlData
-                urlData.h1[i] = $(this).text();
-            })
-            $('h2').each(function(i) {
-
-                if (typeof urlData.h2 === 'undefined') {
-                    urlData.h2 = {};
-                }
-
-                //adds all h1's to urlData
-                urlData.h2[i] = $(this).text();
-            })
-            // By putting our data into JSON, we can decode it in the AJAX response on the frontend by using:
-            // JSON.parse(responseData)
-            // TODO: implement JSON.parse() on frontend to display the urlData object nicely
-            res.send(JSON.stringify(urlData));
-        })
-        .catch(function(error) {
-            console.log(error);
-        })
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
